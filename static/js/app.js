778bf99d7d32a518fc0c8bb251dc3ca2b8193d98
Justin <harwoodjp@g.cofc.edu>
2017-02-09 10:25:05 -0500

    init

---
+++ static/js/app.js
@@ -0,0 +1,3 @@
+require('../css/style.scss');
+require('../css/normalize.css');
+require('../../index.html')
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+	entry: './static/js/app.js',
+	output: {
+		filename: './static/js/bundle.js'
+	},
+
+	watch: true,
+
+	module: {
+		loaders: [
+
+			// html 
+			{
+				test: /\.html$/,
+				loader: "raw-loader"
+			},
+
+			// babel
+			{
+				test: /\.js$/,
+				exclude: /node_modules/, 
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				}
+			},
+
+			// css/style loader
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				loader: 'style-loader!css-loader'
+			},
+
+			// sass
+			{
+			  test: /\.scss$/,
+			  use: ['style-loader', 'css-loader', 'sass-loader'],
+			}
+			
+		]
+	}
+
+};
